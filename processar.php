70473f777e04b9689a1fb1a68451b47a4576e668
Miguel Baggio <migbaggio@gmail.com>
2024-10-22 17:06:31 -0300

    Removi os métodos e estou criando novos, os métodos que eu fiz anteriormente estavam errados.

--- processar.php
+++ processar.php
@@ -6,7 +6,7 @@ if (!isset($_SESSION['senha'])) {
     echo "Nenhuma senha foi fornecida.";
     exit();
 }
-
+$dicionario1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 // Recupera a senha da sessão
 $senha = $_SESSION['senha'];
 $tamanho = mb_strlen($senha); //pega o tamanho da senha.
@@ -19,106 +19,46 @@ echo "<p>A senha que você digitou tem tamanho: <strong>" . $tamanho . "</strong
 
 // Define o fuso horário (opcional, mas recomendado)
 date_default_timezone_set('America/Sao_Paulo');
-echo "<h1>Testando apenas números</h1>"; 
-inteiros($senha,$tamanho);
-echo "<h1>Testando números e letras</h1>"; 
-inteiros_letras($senha,$tamanho);
+echo "<h1>Testando apenas números</h1>";
+inteiros($senha, $dicionario1);
 
-function inteiros($senha,$tamanho){
+
+function inteiros($senha, $tamanho)
+{
     $tempo_inicio = microtime(true); // Obtém a hora atual Formato: Horas:Minutos:Segundos
     echo "<p>A hora que começou o algoritmo:" . $tempo_inicio;
     $resultado = testa_inteiros($senha, $tamanho);
     $tempo_fim = microtime(true); // Obtém a hora atual Formato: Horas:Minutos:Segundos
-    echo "<p>A senha que você digitou é: <strong>" . $resultado . "</strong></p>";
     echo "<p>A hora que terminou o algoritmo:" . $tempo_fim;
     $tempoTotal = $tempo_inicio - $tempo_fim;
-    echo "<p>Total executando o algoritmo:" . round($tempoTotal, 4) ;
-
+    echo "<p>Total executando o algoritmo:" . round($tempoTotal, 4);
 }
 
-
-function inteiros_letras($senha,$tamanho){
-    $tempo_inicio = microtime(true); // Obtém a hora atual Formato: Horas:Minutos:Segundos
-    echo "<p>A hora que começou o algoritmo:" . $tempo_inicio;
-    $resultado = testa_inteiros_letras($senha, $tamanho);
-    $tempo_fim = microtime(true); // Obtém a hora atual Formato: Horas:Minutos:Segundos
-    echo "<p>A senha que você digitou é: <strong>" . $resultado . "</strong></p>";
-    echo "<p>A hora que terminou o algoritmo:" . $tempo_fim;
-    $tempoTotal = $tempo_inicio - $tempo_fim;
-    echo "<p>Total executando o algoritmo:" . round($tempoTotal, 4) ;
-
-}
-
-function testa_inteiros($senha, $tamanho)
+function testa_inteiros($senha, $dicionario)
 {
-    $achei = "";
-    for ($i = 0; $i < $tamanho; $i++) {
-        if ($senha[$i] == 0) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 1) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 2) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 3) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 4) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 5) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 6) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 7) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 8) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 9) {
-            $achei[$i] = $senha[$i];
+    $achou = 0;
+    $aux = "";
+    while ($achou != 1) {
+        testatudo($senha, $dicionario, $aux);
+    }
+}
+
+function testatudo($senha, $dicionario, $aux)
+{
+    $achou = 0;
+    for ($i = 0; $i < sizeof($dicionario); $i++) {
+        $aux = $aux + $dicionario[$i];
+        echo "<p>Testando com senha igual a :" . $aux;
+        if ($senha == $aux) {
+            echo " -> Encontrou, a senha era:" . $aux;
+            $achou = 1;
+            return $achou;
+        } else {
+            echo " -> Não encontrou";
         }
     }
-    return $achei;
+    return $achou;
 }
-
-function testa_inteiros_letras($senha, $tamanho)
-{
-    $achei = "";
-    for ($i = 0; $i < $tamanho; $i++) {
-        if ($senha[$i] == 0) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 1) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 2) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 3) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 4) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 5) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 6) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 7) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 8) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 9) {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 'a') {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 'A') {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 'b') {
-            $achei[$i] = $senha[$i];
-        } else if ($senha[$i] == 'B') {
-            $achei[$i] = $senha[$i];
-        }
-    }
-    return $achei;
-}
-
-
-
-
 
 // Limpa a sessão após o uso
 unset($_SESSION['senha']);
-?>
